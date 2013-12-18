55d03abd7dac8192bab2ac3c57b80a59fe6f8966
Thufir <hawat.thufir@gmail.com>
2013-12-18 12:33:08 -0800

    increment

--- src/ratios/Ratios.java
+++ src/ratios/Ratios.java
@@ -1,19 +1,69 @@
 package ratios;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static java.lang.System.out;
 
 public class Ratios {
 
-    private static String input = "AB: 272/272  CD: 204/529  EFGH: 105 HIJKL: 105  MN: 0 OPQ: 0%";
+    private String input = "AB: 272/272  CD: 204/529  EFGH: 105 HIJKL: 105  MN: 0 OPQ: 0%";
+    private Map<String, String> ms = new HashMap<>();
+    private Map<String, Ratio> mr = new HashMap<>();
 
     public Ratios() {
-        regex();
+        populateMap();
+        ratios();
+        printMap(mr);
     }
 
     public static void main(String[] args) {
         new Ratios();
+    }
+
+    private  void ratios() {
+        Pattern pattern = Pattern.compile("(\\d+)/(\\d+)");
+        Pattern p2 = Pattern.compile("(\\w+)");
+        Matcher m2;
+        int num, den;
+        Ratio ratio = null;
+        for (Map.Entry<String, String> e : ms.entrySet()) {
+            ratio = null;
+            num = 0;
+            den = 1;
+            Matcher matcher = pattern.matcher(e.getValue());
+            while (matcher.find()) {
+                num = Integer.parseInt(matcher.group(1));
+                den = Integer.parseInt(matcher.group(2));
+                ratio = new Ratio(num, den);
+            }
+            if (ratio == null) {
+                m2 = p2.matcher(e.getValue());
+                while (m2.find()) {
+                    num = Integer.parseInt(m2.group());
+                    den = 1;
+                    ratio = new Ratio(num, den);
+                }
+            }
+           mr.put(e.getKey(), ratio);
+        }
+    }
+
+    private void populateMap() {
+        Pattern pattern = Pattern.compile("(\\w+): +(\\S+)");
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            ms.put(matcher.group(1), matcher.group(2));
+        }
+    }
+
+    private void printMap(Map<?, ?> m) {
+        for (Map.Entry<?, ?> e : m.entrySet()) {
+            String key = e.getKey().toString();
+            String val = e.getValue().toString();
+            out.println(key + "\t\t" + val);
+        }
     }
 
     private void regex() {
